38ac5a284b4090a13ed15e260c3083ba151faf3a
alejandra <alejandra.colicheo17@gmail.com>
2020-04-29 12:45:22 -0300

    eliminamos botones y agregamos data

--- src/main.js
+++ src/main.js
@@ -1,6 +1,38 @@
-import { example } from './data.js';
-// import data from './data/lol/lol.js';
-import data from './data/pokemon/pokemon.js';
-// import data from './data/rickandmorty/rickandmorty.js';
+import data from './data/pokemon/pokemon.js'
 
-console.log(example, data);
+for (let i=0; i<data.pokemon.length; i++){
+    //creandp tarjeta
+    let cards= document.createElement("div");
+    cards.className ="cards";
+    cards.id = "cards";
+   //nombre de la tarjeta
+   let name = document.createElement("h3");
+   name.textContent = data.pokemon[i].name;
+   //imagen
+   let image = document.createElement("img");
+   image.src =data.pokemon[i].img;
+   //numero
+   let number = document.createElement("h4");
+   number.textContent ="#" + data.pokemon[i].num;
+   cards.className = "number";
+   //primer tipo de pokemon
+   let type1 = document.createElement ("p");
+   type1.textContent = data.pokemon[i].type[0];
+   type1.className = "type1";
+   //segundo tipo pokemon
+   let type2 = document.createElement ("p");
+   type2.textContent = data.pokemon[i].type[1];
+   type2.className = "type2";
+   //asignando la variables appendchild agrega elementos a una lista
+   cards.appendChild(name);
+   cards.appendChild(image);
+   cards.appendChild(number);
+   cards.appendChild(type1);
+   cards.appendChild(type2);
+   //mostrar la tarjeta en html -> section (el container que hicimos)
+   document.getElementById("container").appendChild(cards).innerHTML;
+
+
+
+}
+
